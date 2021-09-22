464e9e6ddf6ffda45313841c40881a7d15fca8d0
You Hui <youhui@babybus.com>
2021-09-22 22:37:46 +0800

    [Update] window

--- SFShop/Main/AppDelegate.m
+++ SFShop/Main/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainTabViewController.h"
 
 @interface AppDelegate ()
 
@@ -13,12 +14,13 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    self.window = [[UIWindow alloc]initWithFrame:UIScreen.mainScreen.bounds];
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
     self.window.backgroundColor = [UIColor whiteColor];
-    
+    MainTabViewController *tab = [[MainTabViewController alloc] init];
+    tab.viewControllers = @[UIViewController.new,UIViewController.new];
+    self.window.rootViewController = tab;
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ SFShop/Main/MainTabViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainTabViewController.h
+//  SFShop
+//
+//  Created by MasterFly on 2021/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainTabViewController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
