bd103265c74a4e927c0319cd27fd9006eb4ca38f
Jacue <282295491@qq.com>
2021-10-16 10:35:48 +0800

    优化用户信息DB存储，可直接获得Object对象

--- SFShop/Utils/UserModel.m
+++ SFShop/Utils/UserModel.m
@@ -9,8 +9,68 @@
 
 @implementation userResModel
 
+-(void)encodeWithCoder:(NSCoder *)aCoder
+{
+     unsigned int count=0;
+    Ivar *ivars=class_copyIvarList([userResModel class], &count);
+    for (int i=0; i<count; i++) {
+        Ivar ivar=ivars[i];
+        const char *name=ivar_getName(ivar);
+        NSString *key=[NSString stringWithUTF8String:name];
+        [aCoder encodeObject:[self valueForKey:key] forKey:key];
+    }
+    free(ivars);
+}
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self=[super init]) {
+        unsigned int count=0;
+        Ivar *ivars=class_copyIvarList([userResModel class], &count);
+        for (int i=0; i<count; i++) {
+            Ivar ivar=ivars[i];
+            const char *name=ivar_getName(ivar);
+            NSString *key=[NSString stringWithUTF8String:name];
+            id value=[aDecoder decodeObjectForKey:key];
+            [self setValue:value forKey:key];
+        }
+        free(ivars);
+    }
+    return self;
+}
+
 @end
 
 @implementation UserModel
+
+-(void)encodeWithCoder:(NSCoder *)aCoder
+{
+     unsigned int count=0;
+    Ivar *ivars=class_copyIvarList([UserModel class], &count);
+    for (int i=0; i<count; i++) {
+        Ivar ivar=ivars[i];
+        const char *name=ivar_getName(ivar);
+        NSString *key=[NSString stringWithUTF8String:name];
+        [aCoder encodeObject:[self valueForKey:key] forKey:key];
+    }
+    free(ivars);
+}
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self=[super init]) {
+        unsigned int count=0;
+        Ivar *ivars=class_copyIvarList([UserModel class], &count);
+        for (int i=0; i<count; i++) {
+            Ivar ivar=ivars[i];
+            const char *name=ivar_getName(ivar);
+            NSString *key=[NSString stringWithUTF8String:name];
+            id value=[aDecoder decodeObjectForKey:key];
+            [self setValue:value forKey:key];
+        }
+        free(ivars);
+    }
+    return self;
+}
 
 @end
