7f67c171b60c77fa37801ada89506a55a2b752b5
You Hui <youhui@babybus.com>
2021-10-25 23:31:43 +0800

    [update] search init

---
+++ SFShop/Components/Search/SFSearchView.h
@@ -0,0 +1,16 @@
+//
+//  SFSearchView.h
+//  SFShop
+//
+//  Created by MasterFly on 2021/10/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SFSearchView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SFShop/Components/Search/SFSearchViewController.h
@@ -0,0 +1,16 @@
+//
+//  SFSearchViewController.h
+//  SFShop
+//
+//  Created by MasterFly on 2021/10/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SFSearchViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
