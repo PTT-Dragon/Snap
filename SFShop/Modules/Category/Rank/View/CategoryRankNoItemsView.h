aa3b61d0b88e559ded72b4cbb50f1896fe2fe1ab
You Hui <youhui@babybus.com>
2021-12-04 16:01:35 +0800

    [Update] 商品没有检索到视图

---
+++ SFShop/Modules/Category/Rank/View/CategoryRankNoItemsView.h
@@ -0,0 +1,16 @@
+//
+//  CategoryRankNoItemsView.h
+//  SFShop
+//
+//  Created by MasterFly on 2021/12/4.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CategoryRankNoItemsView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
