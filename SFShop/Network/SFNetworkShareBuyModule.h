652396a72f7f278f6a9560e3dec04e6f8d52be65
游挺 <rx_dadoubi@sina.com>
2021-12-10 12:12:51 +0800

    no message

---
+++ SFShop/Network/SFNetworkShareBuyModule.h
@@ -0,0 +1,19 @@
+//
+//  SFNetworkShareBuyModule.h
+//  SFShop
+//
+//  Created by 游挺 on 2021/12/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SFNetworkGroupBuyModule : NSObject
+@property (nonatomic, readonly, copy) NSString *group;
+
+- (NSString *)getGroupBuyGroupNbr: (NSString *)GroupNbr;
+
+@end
+
+NS_ASSUME_NONNULL_END
