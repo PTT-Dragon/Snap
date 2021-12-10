66a2230b1bf87e0352bbd1b6260b6e59e14f38da
游挺 <rx_dadoubi@sina.com>
2021-12-10 10:22:26 +0800

    no message

---
+++ SFShop/Modules/Account/Message/Model/MessageModel.m
@@ -0,0 +1,28 @@
+//
+//  MessageModel.m
+//  SFShop
+//
+//  Created by 游挺 on 2021/12/10.
+//
+
+#import "MessageModel.h"
+
+@implementation MessageUnreadModel
++ (BOOL)propertyIsOptional:(NSString *)propertyName
+{
+    return YES;
+}
+@end
+@implementation MessageContactModel
++ (BOOL)propertyIsOptional:(NSString *)propertyName
+{
+    return YES;
+}
+@end
+
+@implementation MessageModel
++ (BOOL)propertyIsOptional:(NSString *)propertyName
+{
+    return YES;
+}
+@end
