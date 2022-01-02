c631696ec4d3679ac85717c930ead2d85434bfc8
Lufer <627505480@qq.com>
2022-01-02 22:43:56 +0800

    [feat] 添加初始化方法

--- SFShop/Main/AppDelegate.m
+++ SFShop/Main/AppDelegate.m
@@ -7,7 +7,8 @@
 
 #import "AppDelegate.h"
 #import "MainTabViewController.h"
-#import<UMShare/UMShare.h>
+#import <UMShare/UMShare.h>
+#import <UMCommon/UMCommon.h>
 
 @interface AppDelegate ()
 
@@ -42,29 +43,27 @@
 //    }];
 }
 
-//-(BOOL)application:(UIApplication*)application handleOpenURL:(NSURL *)url
-//{
-//    BOOL result =[[UMSocialManager defaultManager] handleOpenURL:url];
-//if(!result){
-//// 其他如支付等SDK的回调
-//}
-//return result;
-//}
+-(BOOL)application:(UIApplication*)application handleOpenURL:(NSURL *)url {
+    BOOL result =[[UMSocialManager defaultManager] handleOpenURL:url];
+    if(!result){
+        // 其他如支付等SDK的回调
+    }
+    return result;
+}
 
 - (void)initUmeng {
-//    +(void)initWithAppkey:(NSString*)appKey channel:(NSString*)channel;
+//    [UMConfigure initWithAppkey:@"Your appkey" channel:@"App Store"];
 }
+
 - (void)confitUShareSettings {
  
 }
 
 - (void)configUSharePlatforms {
-    /* 设置Twitter的appKey和appSecret */
 //    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Twitter appKey:@"fB5tvRpna1CKK97xZUslbxiet"  appSecret:@"YcbSvseLIwZ4hZg9YmgJPP5uWzd4zr6BpBKGZhf07zzh3oj62K" redirectURL:nil];
-    /* 设置Facebook的appKey和UrlString */
 //    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Facebook appKey:@"506027402887373"  appSecret:nil redirectURL:@"http://www.umeng.com/social"];
-
-
+//    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Whatsapp appKey:@"" appSecret:@"" redirectURL:@""];
+//    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Instagram appKey:@"" appSecret:@"" redirectURL:@""];
 }
 
 
