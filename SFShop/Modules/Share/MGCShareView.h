1fab8e2d87ae80a09572ac24326e7e6948204318
Lufer <627505480@qq.com>
2022-01-26 21:46:53 +0800

    [feat] 添加分享页面

--- SFShop/Modules/Share/MGCShareView.h
+++ SFShop/Modules/Share/MGCShareView.h
@@ -6,37 +6,21 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "MGCShareItemModel.h"
 #import "MGCShareInfoModel.h"
-//#import "MGCShareViewModel.h"
-
-
-//分享页面类型
-typedef NS_ENUM(NSInteger, MGCShareViewType) {
-    //正常两行
-    MGCShareViewTypeNormal = 0,
-    //只有分享
-    MGCShareViewTypeOnlyShare,
-    //只有工具
-    MGCShareViewTypeOnlyTool
-};
-
+#import <UShareUI/UShareUI.h>
 
 @interface MGCShareView : UIView
 
 @property (nonatomic, strong) MGCShareInfoModel *shareInfoModel;
 
-@property (nonatomic, copy) void (^successBlock)(NSDictionary *info, MGCShareItemType type);
+@property (nonatomic, copy) void (^successBlock)(NSDictionary *info, UMSocialPlatformType type);
 
-@property (nonatomic, copy) void (^failBlock)(NSDictionary *info, MGCShareItemType type);
+@property (nonatomic, copy) void (^failBlock)(NSDictionary *info, UMSocialPlatformType type);
 
-@property (nonatomic, copy) NSString *contentType;
-
-+ (MGCShareView *)showShareViewWithSuperView:(UIView *)superView
-                              shareInfoModel:(MGCShareInfoModel *)shareInfoModel
-                                successBlock:(void (^)(NSDictionary *info, MGCShareItemType type))successBlock
-                                   failBlock:(void (^)(NSDictionary *info, MGCShareItemType type))failBlock
-                                   completed:(void (^)(BOOL isShow))completed;
++ (MGCShareView *)showShareViewWithShareInfoModel:(MGCShareInfoModel *)shareInfoModel
+                                     successBlock:(void (^)(NSDictionary *info, UMSocialPlatformType type))successBlock
+                                        failBlock:(void (^)(NSDictionary *info, UMSocialPlatformType type))failBlock
+                                        completed:(void (^)(BOOL isShow))completed;
 /// 删除视图
 - (void)cancelBtnAction;
 
