5831bcc09b45a2a035141421fb002da4cafef773
feng.lu <feng.lu@ui.com>
2022-01-26 16:41:46 +0800

    [feat] 修改webview language key

--- SFShop/Components/PublicWebViewController.m
+++ SFShop/Components/PublicWebViewController.m
@@ -144,7 +144,7 @@
     // 设置localStorage
     NSString *token = [NSString stringWithFormat:@"localStorage.setItem('h5Token', '%@')", model.accessToken];
     NSString *isLogin = [NSString stringWithFormat:@"localStorage.setItem('isLogin', '%d')", model ? YES : NO];
-    NSString *language = [NSString stringWithFormat:@"localStorage.setItem('language', '%@')",UserDefaultObjectForKey(@"Language")];
+    NSString *language = [NSString stringWithFormat:@"localStorage.setItem('USER_LANGUAGE', '%@')",UserDefaultObjectForKey(@"Language")];
     [self.webView evaluateJavaScript:token completionHandler:nil];
     [self.webView evaluateJavaScript:isLogin completionHandler:nil];
     [self.webView evaluateJavaScript:language completionHandler:nil];
