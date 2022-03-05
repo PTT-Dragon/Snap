f44338ac4b9c36933f0c4cbd7a818b4b771f4505
Lufer <627505480@qq.com>
2022-03-05 21:54:07 +0800

    优化

--- SFShop/Modules/Share/MGCShareManager.m
+++ SFShop/Modules/Share/MGCShareManager.m
@@ -27,18 +27,48 @@
     [MGCShareView showShareViewWithShareInfoModel:nil
                                      successBlock:^(NSDictionary *info, MGCShareType type) {
         if (type == MGCShareFacebookType) {
-            FBSDKShareLinkContent *content = [[FBSDKShareLinkContent alloc] init];
-            NSString *urlStr = [message stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-            content.contentURL = [NSURL URLWithString:urlStr];
-            
-            FBSDKShareDialog *dialog = [[FBSDKShareDialog alloc] init];
-            dialog.fromViewController = [UIViewController currentTopViewController];
-            dialog.shareContent = content;
-            dialog.mode = FBSDKShareDialogModeAutomatic;
-            [dialog show];
+            [self shareToFaceBookWithMessage:message];
         } else if (type == MGCShareTwitterType) {
-            //检查是否当前会话具有登录的用户
-            if ([[Twitter sharedInstance].sessionStore hasLoggedInUsers]) {
+            [self shareToTwitterWithMessage:message];
+        } else if (type == MGCShareWhatsAppType) {
+            [self shareToWhatsAppWithMessage:message];
+        } else if (type == MGCShareCopyLinkType) {
+            UIPasteboard *pab = [UIPasteboard generalPasteboard];
+            pab.string = message;
+        }
+    }
+                                        failBlock:nil
+                                        completed:nil];
+}
+
+
+- (void)shareToFaceBookWithMessage:(NSString *)message {
+    FBSDKShareLinkContent *content = [[FBSDKShareLinkContent alloc] init];
+    NSString *urlStr = [message stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    content.contentURL = [NSURL URLWithString:urlStr];
+    FBSDKShareDialog *dialog = [[FBSDKShareDialog alloc] init];
+    dialog.fromViewController = [UIViewController currentTopViewController];
+    dialog.shareContent = content;
+    dialog.mode = FBSDKShareDialogModeAutomatic;
+    [dialog show];
+}
+
+- (void)shareToTwitterWithMessage:(NSString *)message {
+    //检查是否当前会话具有登录的用户
+    if ([[Twitter sharedInstance].sessionStore hasLoggedInUsers]) {
+        TWTRComposer *composer = [[TWTRComposer alloc] init];
+        [composer setText:message];
+        [composer showFromViewController:[UIViewController currentTopViewController]
+                              completion:^(TWTRComposerResult result){
+            if(result == TWTRComposerResultCancelled) {
+                //分享失败
+            } else {
+                //分享成功
+            }
+        }];
+    }else{
+        [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession *session, NSError *error) {
+            if (session) {
                 TWTRComposer *composer = [[TWTRComposer alloc] init];
                 [composer setText:message];
                 [composer showFromViewController:[UIViewController currentTopViewController]
@@ -49,38 +79,23 @@
                         //分享成功
                     }
                 }];
-            }else{
-                [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession *session, NSError *error) {
-                    if (session) {
-                        TWTRComposer *composer = [[TWTRComposer alloc] init];
-                        [composer setText:message];
-                        [composer showFromViewController:[UIViewController currentTopViewController]
-                                              completion:^(TWTRComposerResult result){
-                            if(result == TWTRComposerResultCancelled) {
-                                //分享失败
-                            } else {
-                                //分享成功
-                            }
-                        }];
-                        NSLog(@"signed in as %@", [session userName]);
-                    } else {
-                        NSLog(@"error: %@", [error localizedDescription]);
-                    }
-                }];
+                NSLog(@"signed in as %@", [session userName]);
+            } else {
+                NSLog(@"error: %@", [error localizedDescription]);
             }
-        } else if (type == MGCShareWhatsAppType) {
-            NSString *url = [NSString stringWithFormat:@"whatsapp://send?text=%@", [message stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]]];
-            NSURL *whatsappURL = [NSURL URLWithString: url];
-            if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
-                [[UIApplication sharedApplication] openURL: whatsappURL];
-            }
-        } else if (type == MGCShareCopyLinkType) {
-            UIPasteboard *pab = [UIPasteboard generalPasteboard];
-            pab.string = message;
-        }
+        }];
     }
-                                        failBlock:nil
-                                        completed:nil];
 }
+
+- (void)shareToWhatsAppWithMessage:(NSString *)message {
+    NSString *url = [NSString stringWithFormat:@"whatsapp://send?text=%@", [message stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]]];
+    NSURL *whatsappURL = [NSURL URLWithString: url];
+    if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
+        [[UIApplication sharedApplication] openURL: whatsappURL];
+    }
+}
+
+
+
 
 @end
